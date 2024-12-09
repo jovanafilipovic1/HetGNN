78ed2d618da0e29c8780401e9ba98b9fc03f6cd9
jovanafilipovic1 <jovana@hotmail.be>
2024-12-09 18:34:23 +0100

    features + jobscript

--- runPython.sh
+++ runPython.sh
@@ -1,17 +1,50 @@
 #!/bin/bash
-#PBS -N run_python                    ## job name
-#PBS -l nodes=1:ppn=8                ## 1 node, 16 cores
-#PBS -l walltime=04:00:00             ## max. 2h of wall time
+#PBS -N run_python                   ## job name
+#PBS -l nodes=1:ppn=3:gpus=1         ## 1 node, 16 cores
+#PBS -l walltime=00:30:00            ## max. hours of wall time
+#PBS -l mem=8gb                    ## If not used, memory will be available proportional to the max amount
+#PBS -m abe                          ## e-mail notifications (abe=aborted, begin and end)
 
-module swap cluster/joltik
+export PATH=$PATH:/user/gent/458/vsc45895/.local/bin
 
-module load Pytorch 
-moudle load numpy
-module load pandas
-module load wandb
+# load CUDA and Python module first
+module load CUDA-Python/12.1.0-gfbf-2023a-CUDA-12.1.1
+
+# check GCCcore module version
+#module list GCCcore
+
+# we require CMake for installing pyg from source; afterwards we will not need it
+module load CMake/3.26.3-GCCcore-12.3.0
+
+# check cuda version
+#nvcc --version
+
+# install pytorch for the desired cuda version, generate URL at https://pytorch.org/get-started/locally/
+#pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+#python -c "import torch; print(torch.version.cuda); print(torch.__version__)"
+
+# install torch geometric
+#pip3 install torch_geometric
+#python -c "import torch_geometric; print(torch_geometric.__version__)"
+
+# install torch_sparse and torch_scatter for the right torch and cuda version
+#pip3 install torch_sparse torch_scatter -f https://pytorch-geometric.com/whl/torch-2.5.1+cu121.html
+#python -c "import torch_sparse; print(torch_sparse.__version__)"
+#python -c "import torch_scatter; print(torch_scatter.__version__)"
+
+# install pyg_lib from master
+#pip install ninja wheel
+#pip install git+https://github.com/pyg-team/pyg-lib.git
+#python -c "import pyg_lib; print(pyg_lib.__version__)"
+
+
+module load scikit-learn/1.3.1-gfbf-2023a
+module load Seaborn/0.13.2-gfbf-2023a
+module load matplotlib/3.7.2-gfbf-2023a
+#module load wandb/0.16.1-GCC-12.3.0
 
 echo "Module Loading Complete"
 
 cd /data/gent/458/vsc45895/Thesis
 
-python hetgnn_main.py --epochs 30 --lr 0.0001 --batch_size 256 --emb_dim 512 --seed 42 --exp_name example_test --metapaths True
+python3 hetgnn_main.py --epochs 30 --lr 0.0001 --batch_size 256 --emb_dim 512 --seed 42 --exp_name example_test --metapaths True
